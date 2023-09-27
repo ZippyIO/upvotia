baf9df38f500ca9d0842260b6b1264b3db7a47f2
Nick <delta2888@gmail.com>
2023-09-27 16:26:27 +1000

    feat: :sparkles: add ZoneCreatePage

---
+++ src/app/(app)/z/create/page.tsx
@@ -0,0 +1,11 @@
+import NewZoneForm from '~/components/zone/NewZoneForm';
+
+const ZoneCreatePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <NewZoneForm />
+    </main>
+  );
+};
+
+export default ZoneCreatePage;
